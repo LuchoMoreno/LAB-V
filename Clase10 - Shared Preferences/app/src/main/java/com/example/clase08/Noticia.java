d505e199c65f72f3c2ee2fd8764864de94dff6a6
luchomoreno <lucho.moreno@live.com>
2020-12-28 07:05:39 -0300

    Clase10 - Shared Preferences

---
+++ Clase10 - Shared Preferences/app/src/main/java/com/example/clase08/Noticia.java
@@ -0,0 +1,11 @@
+package com.example.clase08;
+
+public class Noticia {
+
+    public String titulo;
+    public String descripcion;
+    public String link;
+    public String urlImg;
+
+    public byte[] img;
+}
